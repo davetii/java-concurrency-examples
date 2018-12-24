bbabf4d03b6948d131a305a6ed819b3ae054eb07
davetii <davetii@gmail.com>
2018-12-24 00:44:40 -0500

    Added join method

--- src/main/java/com/example/concurrency/ThreadExample.java
+++ src/main/java/com/example/concurrency/ThreadExample.java
@@ -1,7 +1,6 @@
 package com.example.concurrency;
 
 class Runner1 extends Thread {
-
     @Override
     public void run() {
         for (int i = 0; i< 10; i++) {System.out.println("Runner1:" + i);}
@@ -16,13 +15,18 @@ class Runner2 extends Thread {
 }
 public class ThreadExample {
 
-    public static void main(String[] args) {
-
-        //Thread t1 = new Thread(new Runner1());
-        //Thread t2 = new Thread(new Runner2());
+    public static void main(String[] args)  {
         Runner1 t1 = new Runner1();
         Runner2 t2 = new Runner2();
         t1.start();
         t2.start();
+
+        try {
+            t1.join();
+            t2.join();
+        } catch (InterruptedException e) {
+            e.printStackTrace();
+        }
+        System.out.println("finish the tasks");
     }
 }
