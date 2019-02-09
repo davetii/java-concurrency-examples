4c37fdb8035ef0eb7eb957ebd851c2661cae4e63
Dave Turner <davetii@gmail.com>
2019-02-08 21:36:12 -0500

    Adding java 8 support refactored ThreadExample

--- src/main/java/com/example/concurrency/ThreadExample.java
+++ src/main/java/com/example/concurrency/ThreadExample.java
@@ -1,32 +1,43 @@
 package com.example.concurrency;
 
-class Runner1 extends Thread {
+
+import java.util.*;
+
+class Runner extends Thread {
+    private List<String> list = new LinkedList<>();
+    public Runner(String[] s) {
+        Collections.addAll(list, s);
+    }
     @Override
     public void run() {
-        for (int i = 0; i< 10; i++) {System.out.println("Runner1:" + i);}
+        list.forEach( System.out::println);
     }
 }
 
-class Runner2 extends Thread {
-    @Override
-    public void run() {
-        for (int i = 0; i< 10; i++) {System.out.println("Runner2:" + i);}
-    }
-}
 public class ThreadExample {
 
     public static void main(String[] args)  {
-        Runner1 t1 = new Runner1();
-        Runner2 t2 = new Runner2();
+        String[] list1 = {"a1", "a2", "a3", "a4", "a5", "a6", "a7", "a8", "a9", "a10"};
+        String[] list2 = {"b1", "b2", "b3", "b4", "b5", "b6", "b7", "b8", "b9", "b10" };
+        Runner t1 = new Runner(list1);
+        Runner t2 = new Runner(list2);
         t1.start();
         t2.start();
 
+        /*
         try {
-            t1.join();
             t2.join();
         } catch (InterruptedException e) {
             e.printStackTrace();
         }
+
+        try {
+            t1.join();
+        } catch (InterruptedException e) {
+            e.printStackTrace();
+        }
+        */
+
         System.out.println("finish the tasks");
     }
 }
